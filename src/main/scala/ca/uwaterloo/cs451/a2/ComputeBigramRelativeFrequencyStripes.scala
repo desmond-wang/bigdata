5c33b47648ed3d35c5b775ecbd3d1ec3da08f404
wang desmond <desmond.wang@uwaterloo.ca>
2019-09-30 05:57:11 -0400

    testing relativeFrequency

--- src/main/scala/ca/uwaterloo/cs451/a2/ComputeBigramRelativeFrequencyStripes.scala
+++ src/main/scala/ca/uwaterloo/cs451/a2/ComputeBigramRelativeFrequencyStripes.scala
@@ -1,18 +1,3 @@
-/**
-  * Bespin: reference implementations of "big data" algorithms
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
 
 package ca.uwaterloo.cs451.a2
 
@@ -25,7 +10,7 @@ import org.apache.spark.SparkConf
 import org.rogach.scallop._
 
 class Conf(args: Seq[String]) extends ScallopConf(args) {
-  mainOptions = Seq(input, output, reducers)
+  mainOptions = Seq(numExecutors, executorCores, executorMemory, input, output, reducers)
   val input = opt[String](descr = "input path", required = true)
   val output = opt[String](descr = "output path", required = true)
   val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
