fc5649da80a20f5d2887aa487c4d5ca17ac9d69c
wang desmond <desmond.wang@uwaterloo.ca>
2019-10-29 06:12:19 -0400

    bug testing

--- src/main/java/ca/uwaterloo/cs451/a4/BuildPersonalizedPageRankRecords.java
+++ src/main/java/ca/uwaterloo/cs451/a4/BuildPersonalizedPageRankRecords.java
@@ -1,3 +1,19 @@
+/**
+ * Bespin: reference implementations of "big data" algorithms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ca.uwaterloo.cs451.a4;
 
 import org.apache.commons.cli.CommandLine;
@@ -29,166 +45,181 @@ import tl.lin.data.array.ArrayListOfFloatsWritable;
 import java.io.IOException;
 import java.util.Arrays;
 
+/**
+ * <p>
+ * Driver program that takes a plain-text encoding of a directed graph and builds corresponding
+ * Hadoop structures for representing the graph.
+ * </p>
+ **/
+
 public class BuildPersonalizedPageRankRecords extends Configured implements Tool {
-    private static final Logger LOG = Logger.getLogger(BuildPersonalizedPageRankRecords.class);
+  private static final Logger LOG = Logger.getLogger(BuildPersonalizedPageRankRecords.class);
 
-    private static final String NODE_CNT_FIELD = "node.cnt";
-    private static final String MULTI_SOURCE = "multi.source";
+  private static final String NODE_CNT_FIELD = "node.cnt";
+  private static final String SOURCE_NODES_FIELD = "source.nodes";
 
-    private static class MyMapper extends Mapper<LongWritable, Text, IntWritable, PageRankNode> {
-        private static final IntWritable nid = new IntWritable();
-        private static final PageRankNode node = new PageRankNode();
-        private static int[] intSources;
+  private static class MyMapper extends Mapper<LongWritable, Text, IntWritable, PageRankNode> {
+    private static final IntWritable nid = new IntWritable();
+    private static final PageRankNode node = new PageRankNode();
+    private static int[] sourceID;
 
-        @Override
-        public void setup(Mapper<LongWritable, Text, IntWritable, PageRankNode>.Context context) {
-            int n = context.getConfiguration().getInt(NODE_CNT_FIELD, 0);
-            if (n == 0) {
-                throw new RuntimeException(NODE_CNT_FIELD + " cannot be 0!");
-            }
+    @Override
+    public void setup(Mapper<LongWritable, Text, IntWritable, PageRankNode>.Context context) {
+      int n = context.getConfiguration().getInt(NODE_CNT_FIELD, 0);
+      if (n == 0) {
+        throw new RuntimeException(NODE_CNT_FIELD + " cannot be 0!");
+      }
 
-            String[] stringSources = context.getConfiguration().getStrings(MULTI_SOURCE);
-            int size = stringSources.length;
-            intSources = new int[size];
-            for (int i = 0; i < size; i++)
-                intSources[i] = Integer.parseInt(stringSources[i]);
+      String[] sourceList = context.getConfiguration().getStrings(SOURCE_NODES_FIELD);
+      int total = sourceList.length;
+	    sourceID = new int[total];
+	    for (int i = 0; i < total; i++) {
+	      sourceID[i] = Integer.parseInt(sourceList[i]);
+	    }
 
-            node.setType(PageRankNode.Type.Complete);
-        }
-
-        @Override
-        public void map(LongWritable key, Text t, Context context) throws IOException,
-                InterruptedException {
-            String[] arr = t.toString().trim().split("\\s+");
-
-            nid.set(Integer.parseInt(arr[0]));
-
-            ArrayListOfFloatsWritable pagerank = new ArrayListOfFloatsWritable();
-            for (int intSource : intSources) {
-                if (Integer.parseInt(arr[0]) == intSource) {
-                    pagerank.add(0.0f);
-                } else {
-                    pagerank.add(Float.NEGATIVE_INFINITY);
-                }
-            }
-            node.setPageRank(pagerank);
-
-            if (arr.length == 1) {
-                node.setNodeId(Integer.parseInt(arr[0]));
-                node.setAdjacencyList(new ArrayListOfIntsWritable());
-
-            } else {
-                node.setNodeId(Integer.parseInt(arr[0]));
-
-                int[] neighbors = new int[arr.length - 1];
-                for (int i = 1; i < arr.length; i++) {
-                    neighbors[i - 1] = Integer.parseInt(arr[i]);
-                }
-
-                node.setAdjacencyList(new ArrayListOfIntsWritable(neighbors));
-            }
-
-            context.getCounter("graph", "numNodes").increment(1);
-            context.getCounter("graph", "numEdges").increment(arr.length - 1);
-
-            if (arr.length > 1) {
-                context.getCounter("graph", "numActiveNodes").increment(1);
-            }
-
-            context.write(nid, node);
-        }
+      node.setType(PageRankNode.Type.Complete);
     }
 
-    public BuildPersonalizedPageRankRecords() {}
+    @Override
+    public void map(LongWritable key, Text t, Context context) throws IOException,
+        InterruptedException {
+      String[] arr = t.toString().trim().split("\\s+");
+      int nodeID = Integer.parseInt(arr[0]);
+      nid.set(nodeID);
 
-    private static final String INPUT = "input";
-    private static final String OUTPUT = "output";
-    private static final String NUM_NODES = "numNodes";
-    private static final String SOURCES = "sources";
+      // set PR mass as 1 for source nodes
+      // set PR mass as 0 for other nodes
+      // but need to take log value
+      ArrayListOfFloatsWritable mass = new ArrayListOfFloatsWritable();
+      for (int sid : sourceID) {
+        if (nodeID == sid) {
+          mass.add(0.0f);
+        } else {
+          mass.add(Float.NEGATIVE_INFINITY);
+        }
+      }
+      node.setPageRank(mass);
 
-    /**
-     * Runs this tool.
-     */
-    @SuppressWarnings({ "static-access" })
-    public int run(String[] args) throws Exception {
-        Options options = new Options();
+      if (arr.length == 1) {
+        node.setNodeId(Integer.parseInt(arr[0]));
+        node.setAdjacencyList(new ArrayListOfIntsWritable());
 
-        options.addOption(OptionBuilder.withArgName("path").hasArg()
-                .withDescription("input path").create(INPUT));
-        options.addOption(OptionBuilder.withArgName("path").hasArg()
-                .withDescription("output path").create(OUTPUT));
-        options.addOption(OptionBuilder.withArgName("num").hasArg()
-                .withDescription("number of nodes").create(NUM_NODES));
-        options.addOption(OptionBuilder.withArgName("num").hasArg()
-                .withDescription("number of sources").create(SOURCES));
+      } else {
+        node.setNodeId(Integer.parseInt(arr[0]));
 
-        CommandLine cmdline;
-        CommandLineParser parser = new GnuParser();
-
-        try {
-            cmdline = parser.parse(options, args);
-        } catch (ParseException exp) {
-            System.err.println("Error parsing command line: " + exp.getMessage());
-            return -1;
+        int[] neighbors = new int[arr.length - 1];
+        for (int i = 1; i < arr.length; i++) {
+          neighbors[i - 1] = Integer.parseInt(arr[i]);
         }
 
-        if (!cmdline.hasOption(INPUT) || !cmdline.hasOption(OUTPUT) || !cmdline.hasOption(NUM_NODES) || !cmdline.hasOption(SOURCES)) {
-            System.out.println("args: " + Arrays.toString(args));
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.setWidth(120);
-            formatter.printHelp(this.getClass().getName(), options);
-            ToolRunner.printGenericCommandUsage(System.out);
-            return -1;
-        }
+        node.setAdjacencyList(new ArrayListOfIntsWritable(neighbors));
+      }
 
-        String inputPath = cmdline.getOptionValue(INPUT);
-        String outputPath = cmdline.getOptionValue(OUTPUT);
-        int n = Integer.parseInt(cmdline.getOptionValue(NUM_NODES));
-        String sources = cmdline.getOptionValue(SOURCES);
+      context.getCounter("graph", "numNodes").increment(1);
+      context.getCounter("graph", "numEdges").increment(arr.length - 1);
 
-        LOG.info("Tool name: " + BuildPersonalizedPageRankRecords.class.getSimpleName());
-        LOG.info(" - inputDir: " + inputPath);
-        LOG.info(" - outputDir: " + outputPath);
-        LOG.info(" - numNodes: " + n);
-        LOG.info("- sources: " + sources);
+      if (arr.length > 1) {
+        context.getCounter("graph", "numActiveNodes").increment(1);
+      }
 
-        Configuration conf = getConf();
-        conf.setInt(NODE_CNT_FIELD, n);
-        conf.setStrings(MULTI_SOURCE, sources);
-        conf.setInt("mapred.min.split.size", 1024 * 1024 * 1024);
+      context.write(nid, node);
+    }
+  }
 
-        Job job = Job.getInstance(conf);
-        job.setJobName(BuildPersonalizedPageRankRecords.class.getSimpleName() + ":" + inputPath);
-        job.setJarByClass(BuildPersonalizedPageRankRecords.class);
+  public BuildPersonalizedPageRankRecords() {}
 
-        job.setNumReduceTasks(0);
+  private static final String INPUT = "input";
+  private static final String OUTPUT = "output";
+  private static final String NUM_NODES = "numNodes";
+  private static final String SOURCES = "sources";
 
-        FileInputFormat.addInputPath(job, new Path(inputPath));
-        FileOutputFormat.setOutputPath(job, new Path(outputPath));
+  /**
+   * Runs this tool.
+   */
+  @SuppressWarnings({ "static-access" })
+  public int run(String[] args) throws Exception {
+    Options options = new Options();
 
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+    options.addOption(OptionBuilder.withArgName("path").hasArg()
+        .withDescription("input path").create(INPUT));
+    options.addOption(OptionBuilder.withArgName("path").hasArg()
+        .withDescription("output path").create(OUTPUT));
+    options.addOption(OptionBuilder.withArgName("num").hasArg()
+        .withDescription("number of nodes").create(NUM_NODES));
+    options.addOption(OptionBuilder.withArgName("source").hasArg()
+        .withDescription("source nodes").create(SOURCES));
 
-        job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(PageRankNode.class);
+    CommandLine cmdline;
+    CommandLineParser parser = new GnuParser();
 
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(PageRankNode.class);
-
-        job.setMapperClass(MyMapper.class);
-
-        // Delete the output directory if it exists already.
-        FileSystem.get(conf).delete(new Path(outputPath), true);
-
-        job.waitForCompletion(true);
-
-        return 0;
+    try {
+      cmdline = parser.parse(options, args);
+    } catch (ParseException exp) {
+      System.err.println("Error parsing command line: " + exp.getMessage());
+      return -1;
     }
 
-    /**
-     * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
-     */
-    public static void main(String[] args) throws Exception {
-        ToolRunner.run(new BuildPersonalizedPageRankRecords(), args);
+    if (!cmdline.hasOption(INPUT) || !cmdline.hasOption(OUTPUT) || 
+    		!cmdline.hasOption(NUM_NODES) || !cmdline.hasOption(SOURCES)) {
+      System.out.println("args: " + Arrays.toString(args));
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.setWidth(120);
+      formatter.printHelp(this.getClass().getName(), options);
+      ToolRunner.printGenericCommandUsage(System.out);
+      return -1;
     }
+
+    String inputPath = cmdline.getOptionValue(INPUT);
+    String outputPath = cmdline.getOptionValue(OUTPUT);
+    int n = Integer.parseInt(cmdline.getOptionValue(NUM_NODES));
+    String sources = cmdline.getOptionValue(SOURCES);
+
+    LOG.info("Tool name: " + BuildPersonalizedPageRankRecords.class.getSimpleName());
+    LOG.info(" - inputDir: " + inputPath);
+    LOG.info(" - outputDir: " + outputPath);
+    LOG.info(" - numNodes: " + n);
+    LOG.info(" - sources: " + sources);
+
+    Configuration conf = getConf();
+    conf.setInt(NODE_CNT_FIELD, n);
+    conf.setStrings(SOURCE_NODES_FIELD, sources);
+    conf.setInt("mapred.min.split.size", 1024 * 1024 * 1024);
+
+    Job job = Job.getInstance(conf);
+    job.setJobName(BuildPersonalizedPageRankRecords.class.getSimpleName() + ":" + inputPath);
+    job.setJarByClass(BuildPersonalizedPageRankRecords.class);
+
+    job.setNumReduceTasks(0);
+
+    FileInputFormat.addInputPath(job, new Path(inputPath));
+    FileOutputFormat.setOutputPath(job, new Path(outputPath));
+
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(SequenceFileOutputFormat.class);
+
+    job.setMapOutputKeyClass(IntWritable.class);
+    job.setMapOutputValueClass(PageRankNode.class);
+
+    job.setOutputKeyClass(IntWritable.class);
+    job.setOutputValueClass(PageRankNode.class);
+
+    job.setMapperClass(MyMapper.class);
+
+    // Delete the output directory if it exists already.
+    FileSystem.get(conf).delete(new Path(outputPath), true);
+
+    job.waitForCompletion(true);
+
+    return 0;
+  }
+
+  /**
+   * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
+   *
+   * @param args command-line arguments
+   * @throws Exception if tool encounters an exception
+   */
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new BuildPersonalizedPageRankRecords(), args);
+  }
 }
--- src/main/java/ca/uwaterloo/cs451/a4/PageRankNode.java
+++ src/main/java/ca/uwaterloo/cs451/a4/PageRankNode.java
@@ -1,3 +1,19 @@
+/**
+ * Bespin: reference implementations of "big data" algorithms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ca.uwaterloo.cs451.a4;
 
 import org.apache.hadoop.io.Writable;
@@ -12,150 +28,155 @@ import java.io.DataOutput;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+/**
+ * Representation of a graph node for PageRank.
+ **/
 public class PageRankNode implements Writable {
-    public static enum Type {
-        Complete((byte) 0),  // PageRank mass and adjacency list.
-        Mass((byte) 1),      // PageRank mass only.
-        Structure((byte) 2); // Adjacency list only.
+  public static enum Type {
+    Complete((byte) 0),  // PageRank mass and adjacency list.
+    Mass((byte) 1),      // PageRank mass only.
+    Structure((byte) 2); // Adjacency list only.
 
-        public byte val;
+    public byte val;
 
-        private Type(byte v) {
-            this.val = v;
-        }
-    };
+    private Type(byte v) {
+      this.val = v;
+    }
+  };
 
-    private static final Type[] mapping = new Type[] { Type.Complete, Type.Mass, Type.Structure };
+  private static final Type[] mapping = new Type[] { Type.Complete, Type.Mass, Type.Structure };
 
-    private Type type;
-    private int nodeid;
-    private ArrayListOfFloatsWritable pagerank;
-    private ArrayListOfIntsWritable adjacenyList;
+  private Type type;
+  private int nodeid;
+  private ArrayListOfFloatsWritable pagerank; // for multi-source
+  private ArrayListOfIntsWritable adjacenyList;
 
-    public PageRankNode() {}
+  public PageRankNode() {}
 
-    public ArrayListOfFloatsWritable getPageRank() {
-        return pagerank;
+  public ArrayListOfFloatsWritable getPageRank() {
+    return pagerank;
+  }
+
+  public void setPageRank(ArrayListOfFloatsWritable p) {
+    this.pagerank = p;
+  }
+
+  public int getNodeId() {
+    return nodeid;
+  }
+
+  public void setNodeId(int n) {
+    this.nodeid = n;
+  }
+
+  public ArrayListOfIntsWritable getAdjacenyList() {
+    return adjacenyList;
+  }
+
+  public void setAdjacencyList(ArrayListOfIntsWritable list) {
+    this.adjacenyList = list;
+  }
+
+  public Type getType() {
+    return type;
+  }
+
+  public void setType(Type type) {
+    this.type = type;
+  }
+
+  /**
+   * Deserializes this object.
+   *
+   * @param in source for raw byte representation
+   * @throws IOException if any exception is encountered during object deserialization
+   */
+  @Override
+  public void readFields(DataInput in) throws IOException {
+    int b = in.readByte();
+    type = mapping[b];
+    nodeid = in.readInt();
+
+    if (type.equals(Type.Mass)) {
+      pagerank = new ArrayListOfFloatsWritable();
+      pagerank.readFields(in);
+      return;
     }
 
-    public void setPageRank(ArrayListOfFloatsWritable p) {
-        this.pagerank = p;
+    if (type.equals(Type.Complete)) {
+      pagerank = new ArrayListOfFloatsWritable();
+      pagerank.readFields(in);
     }
 
-    public int getNodeId() {
-        return nodeid;
+    adjacenyList = new ArrayListOfIntsWritable();
+    adjacenyList.readFields(in);
+  }
+
+  /**
+   * Serializes this object.
+   *
+   * @param out where to write the raw byte representation
+   * @throws IOException if any exception is encountered during object serialization
+   */
+  @Override
+  public void write(DataOutput out) throws IOException {
+    out.writeByte(type.val);
+    out.writeInt(nodeid);
+
+    if (type.equals(Type.Mass)) {
+      pagerank.write(out);
+      return;
     }
 
-    public void setNodeId(int n) {
-        this.nodeid = n;
+    if (type.equals(Type.Complete)) {
+      pagerank.write(out);
     }
 
-    public ArrayListOfIntsWritable getAdjacenyList() {
-        return adjacenyList;
-    }
+    adjacenyList.write(out);
+  }
 
-    public void setAdjacencyList(ArrayListOfIntsWritable list) {
-        this.adjacenyList = list;
-    }
+  @Override
+  public String toString() {
+    return String.format("{%d %s %s}", nodeid, pagerank.toString(50), (adjacenyList == null ? "[]"
+        : adjacenyList.toString(10)));
+  }
 
-    public Type getType() {
-        return type;
-    }
+  /**
+   * Returns the serialized representation of this object as a byte array.
+   *
+   * @return byte array representing the serialized representation of this object
+   * @throws IOException if any exception is encountered during object serialization
+   */
+  public byte[] serialize() throws IOException {
+    ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+    DataOutputStream dataOut = new DataOutputStream(bytesOut);
+    write(dataOut);
 
-    public void setType(Type type) {
-        this.type = type;
-    }
+    return bytesOut.toByteArray();
+  }
 
-    /**
-     * Deserializes this object.
-     *
-     * @param in source for raw byte representation
-     */
-    @Override
-    public void readFields(DataInput in) throws IOException {
-        int b = in.readByte();
-        type = mapping[b];
-        nodeid = in.readInt();
+  /**
+   * Creates object from a <code>DataInput</code>.
+   *
+   * @param in source for reading the serialized representation
+   * @return newly-created object
+   * @throws IOException if any exception is encountered during object deserialization
+   */
+  public static PageRankNode create(DataInput in) throws IOException {
+    PageRankNode m = new PageRankNode();
+    m.readFields(in);
 
-        if (type.equals(Type.Mass)) {
-            pagerank = new ArrayListOfFloatsWritable();
-            pagerank.readFields(in);
-            return;
-        }
+    return m;
+  }
 
-        if (type.equals(Type.Complete)) {
-            pagerank = new ArrayListOfFloatsWritable();
-            pagerank.readFields(in);
-        }
-
-        adjacenyList = new ArrayListOfIntsWritable();
-        adjacenyList.readFields(in);
-    }
-
-    /**
-     * Serializes this object.
-     *
-     * @param out where to write the raw byte representation
-     */
-    @Override
-    public void write(DataOutput out) throws IOException {
-        out.writeByte(type.val);
-        out.writeInt(nodeid);
-
-        if (type.equals(Type.Mass)) {
-            pagerank.write(out);
-            return;
-        }
-
-        if (type.equals(Type.Complete)) {
-            pagerank.write(out);
-        }
-
-        adjacenyList.write(out);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("{%d %s %s}", nodeid, pagerank.toString(30), (adjacenyList == null ? "[]"
-                : adjacenyList.toString(10)));
-    }
-
-    /**
-     * Returns the serialized representation of this object as a byte array.
-     *
-     * @return byte array representing the serialized representation of this object
-     * @throws IOException
-     */
-    public byte[] serialize() throws IOException {
-        ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
-        DataOutputStream dataOut = new DataOutputStream(bytesOut);
-        write(dataOut);
-
-        return bytesOut.toByteArray();
-    }
-
-    /**
-     * Creates object from a <code>DataInput</code>.
-     *
-     * @param in source for reading the serialized representation
-     * @return newly-created object
-     * @throws IOException
-     */
-    public static PageRankNode create(DataInput in) throws IOException {
-        PageRankNode m = new PageRankNode();
-        m.readFields(in);
-
-        return m;
-    }
-
-    /**
-     * Creates object from a byte array.
-     *
-     * @param bytes raw serialized representation
-     * @return newly-created object
-     * @throws IOException
-     */
-    public static PageRankNode create(byte[] bytes) throws IOException {
-        return create(new DataInputStream(new ByteArrayInputStream(bytes)));
-    }
+  /**
+   * Creates object from a byte array.
+   *
+   * @param bytes raw serialized representation
+   * @return newly-created object
+   * @throws IOException if any exception is encountered during object deserialization
+   */
+  public static PageRankNode create(byte[] bytes) throws IOException {
+    return create(new DataInputStream(new ByteArrayInputStream(bytes)));
+  }
 }
